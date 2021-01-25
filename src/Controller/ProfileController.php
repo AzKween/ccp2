fd1463a7c6d02a384ea1fceba1a602021e8b1ba8
D'hornes Rémi <dhornes@hotmail.fr>
2021-01-25 15:38:18 +0100

    Page Profile fini

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,12 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\ProfileType;
 use App\Repository\SiteRepository;
 use App\Repository\KindsRepository;
 use App\Repository\CategoriesRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
 
 class ProfileController extends AbstractController
 {
@@ -18,6 +23,35 @@ class ProfileController extends AbstractController
     {
         return $this->render('profile/index.html.twig', [
             'controller_name' => 'ProfileController',
+            'sites' => $siteRepository->findAll(),
+            'categories' => $categoriesRepository->findAll(),
+            'kinds' => $kindsRepository->findAll(),
+        ]);
+    }
+    /**
+     * @Route("/profile/edit/{id}", name="profile_edit")
+     */
+    public function edit(Request $request, SiteRepository $siteRepository, User $user, KindsRepository $kindsRepository, CategoriesRepository $categoriesRepository, UserPasswordEncoderInterface $passwordEncoder): Response
+    {
+        $form = $this->createForm(ProfileType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $user->setpassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('password')->getData()
+                ));
+
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('profile');
+        }
+        return $this->render('profile/edit.html.twig', [
+            'controller_name' => 'ProfileController',
+            'user' =>$user,
+            'ProfileForm' => $form->createView(),
             'sites' => $siteRepository->findAll(),
             'categories' => $categoriesRepository->findAll(),
             'kinds' => $kindsRepository->findAll(),
---
+++ src/Form/ProfileType.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class ProfileType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class,[
+                'disabled'=>true,
+            ])
+            ->add('password', PasswordType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
+            ->add('LastName', TextType::class, [
+                'disabled'=>true,
+            ])
+            ->add('FirstName', TextType::class, [
+                'disabled'=>true,
+            ])
+            ->add('DateOfBirth', DateType::class, [
+                'widget' => 'single_text',
+                'disabled'=>true,
+            ])
+            ->add('Address')
+            ->add('Zip')
+            ->add('City')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
