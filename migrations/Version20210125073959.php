0eea7e9d9985890dd23b60694148a5ba58ffc9d3
D'hornes Rémi <dhornes@hotmail.fr>
2021-01-25 10:13:51 +0100

    Total Panier OK

---
+++ migrations/Version20210125073959.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210125073959 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE cart ADD relation_cart_id INT NOT NULL');
+        $this->addSql('ALTER TABLE cart ADD CONSTRAINT FK_BA388B73AEDFCE FOREIGN KEY (relation_cart_id) REFERENCES cart (id)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_BA388B73AEDFCE ON cart (relation_cart_id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE cart DROP FOREIGN KEY FK_BA388B73AEDFCE');
+        $this->addSql('DROP INDEX UNIQ_BA388B73AEDFCE ON cart');
+        $this->addSql('ALTER TABLE cart DROP relation_cart_id');
+    }
+}
---
+++ migrations/Version20210125080149.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210125080149 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE cart DROP FOREIGN KEY FK_BA388B7BE15C1E4');
+        $this->addSql('DROP INDEX UNIQ_BA388B7BE15C1E4 ON cart');
+        $this->addSql('ALTER TABLE cart DROP relation_user_id');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE cart ADD relation_user_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE cart ADD CONSTRAINT FK_BA388B7BE15C1E4 FOREIGN KEY (relation_user_id) REFERENCES user (id)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_BA388B7BE15C1E4 ON cart (relation_user_id)');
+    }
+}
